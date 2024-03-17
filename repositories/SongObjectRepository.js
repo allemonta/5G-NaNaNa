b346f556ee8d4e5a8bd34299b41a18f825b86818
Alessandro Montanari <alessandro.montanari@soluzionifutura.it>
2024-03-17 10:09:12 +0100

    Add hint in SongObjectRepository

--- repositories/SongObjectRepository.js
+++ repositories/SongObjectRepository.js
@@ -2,22 +2,29 @@
 const randomId = () => Math.floor(Math.random() * 10000)
 const songs = []
 
+
+// return the whole `songs` list
 export const getAllSongs = () => {
 
 }
 
+// use the .find method applied to the `songs` list
 export const getSongById = (id) => {
 
 }
 
+// generate a random id and add the song data with the new id to the `songs` list
 export const addSong = (songData) => {
 
 }
 
+
+// use the getSongById() method and, if exists, replace with the new songData value
 export const updateSong = (id, songData) => {
 
 }
 
+// use the getSongById() method with the updateSong() method
 export const markAsFavourite = (id, isFavourite) => {
 
 }
