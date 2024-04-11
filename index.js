1f1b78a3b295c42243f0459e4fa11336921aa8d9
Alessandro Montanari <alessandro.montanari@soluzionifutura.it>
2024-04-11 21:25:25 +0200

    Impelementations

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const songRepository = require("./repositories/SongObjectRepository")
 
 const app = Express()
 
+app.use(Express.json())
 
 app.get("/songs", (req, res) => {
-    // use songRepository to implement
-    res.status(501).send("Not implemented yet")
+    const songs = songRepository.getAllSongs()
+    res.send(songs)
 })
 
 app.get("/songs/:id", (req, res) => {
@@ -14,9 +15,30 @@ app.get("/songs/:id", (req, res) => {
     res.status(501).send("Not implemented yet")
 })
 
+// endpoint per la creazione di una nuova canzone
 app.post("/songs", (req, res) => {
-    // use songRepository to implement
-    res.status(501).send("Not implemented yet")
+    const title = req.body.title
+    const imageUrl = req.body.imageUrl
+    const link = req.body.link
+    const favourite = req.body.favourite
+
+    if (
+        (title === undefined) || 
+        (imageUrl === undefined) || 
+        (link === undefined) || 
+        (favourite === undefined)
+    ) {
+        res.status(400).send("Missing field in request")
+    } else {
+        songRepository.addSong({
+            title: title,
+            imageUrl: imageUrl,
+            link: link,
+            favourite: favourite
+        })
+
+        res.status(201).send("Song created")
+    }
 })
 
 app.post("/songs/:id", (req, res) => {
--- repositories/SongObjectRepository.js
+++ repositories/SongObjectRepository.js
@@ -4,27 +4,85 @@ const songs = []
 
 
 // return the whole `songs` list
-export const getAllSongs = () => {
+const getAllSongs = () => {
+    return songs
+}
 
+const getIndexById = (id) => {
+    return songs.findIndex((song) => {
+        return song.id = id
+    })
 }
 
 // use the .find method applied to the `songs` list
-export const getSongById = (id) => {
+const getSongById = (id) => {
+    const index = getIndexById(id)
 
+    if (index === -1) {
+        return null
+    }
+
+    return songs[index]
+
+    // for (let i = 0; i< songs.length; i++) {
+    //     if (songs[i].id === id) {
+    //         return songs[id]
+    //     }
+    // }
+
+    // for (let i in songs) {
+    //     if (songs[i].id === id) {
+    //         return songs[id]
+    //     }
+    // }
+
+    // for (let song of songs) {
+    //     if (song.id === id) {
+    //         return songs[id]
+    //     }
+    // }
+
+    // return null
 }
 
 // generate a random id and add the song data with the new id to the `songs` list
-export const addSong = (songData) => {
-
+const addSong = (songData) => {
+    songData.id = randomId()
+    songs.push(songData)
 }
 
 
 // use the getSongById() method and, if exists, replace with the new songData value
-export const updateSong = (id, songData) => {
+const updateSong = (id, songData) => {
+    songData.id = id
 
+    const idx = getIndexById(id)
+    if (idx !== -1) {
+        songs[idx] = songData
+    }
 }
 
 // use the getSongById() method with the updateSong() method
-export const markAsFavourite = (id, isFavourite) => {
-
+const markAsFavourite = (id, isFavourite) => {
+    const song = getSongById(id)
+    if (song) {
+        song.isFavourite = isFavourite
+        updateSong(song.id, song)
+    }
 }
+
+module.exports = {
+    markAsFavourite,
+    updateSong,
+    getAllSongs,
+    getSongById,
+    addSong,
+    randomId
+}
+
+
+
+
+
+
+
